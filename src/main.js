5d83ff97124f58f08abb9f185b1a82ef42d246ef
FerNenuMendez <fernando.modesto.mendez@gmail.com>
2024-02-13 01:57:03 -0300

    agregado de apiRouter al main

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import express from "express";
 import handlebars from 'express-handlebars'
 import { PORT } from "./utils/config/config.js";
 import { webRouter } from "./routes/web/webRouter.js";
+import { apiRouter } from "./routes/api/apiRouter.js";
 
 const app = express()
 app.use(express.json())
@@ -14,3 +15,4 @@ const server = app.listen(PORT, () => {
 
 app.use('/static', express.static('./static'))
 app.use(webRouter)
+app.use('/', apiRouter)
