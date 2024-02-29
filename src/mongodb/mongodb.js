04fbb6b9fc8741c039742610d702f2fc30e6e5b7
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-02-29 18:22:26 -0300

    C String

--- src/mongodb/mongodb.js
+++ src/mongodb/mongodb.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const ATLAS_STRING = process.env.ATLAS_STRING;
-console.log(ATLAS_STRING)
-await mongoose.connect(ATLAS_STRING)
+
+await mongoose.connect(ATLAS_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
 
 export { RegistroManager } from '../models/registros.js'
